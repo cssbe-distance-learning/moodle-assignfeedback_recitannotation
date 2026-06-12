4585f6779d06d0e0a479119bdd54d8b5bea77fd9
Gustavo Bazzo <gcbazzo@gmail.com>
2026-06-12 15:05:43 -0400

    PHPUnit tests

---
+++ src/tests/persistctrl_test.php
@@ -0,0 +1,243 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   assignfeedback_recitannotation
+ * @copyright 2025 RECIT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace assignfeedback_recitannotation\tests;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/mod/assign/feedback/recitannotation/locallib.php');
+
+/**
+ * Tests for PersistCtrl criteria and comments CRUD.
+ *
+ * @covers \recitannotation\PersistCtrl
+ */
+class persistctrl_test extends \advanced_testcase {
+
+    /** @var \stdClass */
+    private $assign;
+
+    /** @var \recitannotation\PersistCtrl */
+    private $ctrl;
+
+    protected function setUp(): void {
+        global $DB, $USER;
+        parent::setUp();
+        $this->resetAfterTest(true);
+
+        // PersistCtrl is a singleton; reset it so each test gets a fresh instance
+        // bound to the current test's $DB and $USER.
+        $ref = new \ReflectionProperty(\recitannotation\PersistCtrl::class, 'instance');
+        $ref->setAccessible(true);
+        $ref->setValue(null, null);
+
+        $generator = $this->getDataGenerator();
+        $course   = $generator->create_course();
+        $teacher  = $generator->create_user();
+        $generator->enrol_user($teacher->id, $course->id, 'editingteacher');
+        $this->setUser($teacher);
+
+        $assigngen    = $generator->get_plugin_generator('mod_assign');
+        $this->assign = $assigngen->create_instance(['course' => $course->id]);
+
+        $this->ctrl = \recitannotation\PersistCtrl::getInstance($DB, $USER);
+    }
+
+    // -----------------------------------------------------------------------
+    // Criteria
+    // -----------------------------------------------------------------------
+
+    public function test_save_criterion_returns_positive_id(): void {
+        $saved = $this->ctrl->saveCriterion($this->makeCriterion('clarity', 1));
+        $this->assertGreaterThan(0, $saved->id);
+    }
+
+    public function test_get_criteria_list_returns_saved_items(): void {
+        $this->ctrl->saveCriterion($this->makeCriterion('clarity', 1));
+        $this->ctrl->saveCriterion($this->makeCriterion('grammar', 2));
+
+        $list = $this->ctrl->getCriteriaList($this->assign->id);
+        $this->assertCount(2, $list);
+    }
+
+    public function test_criteria_returned_in_sort_order(): void {
+        $this->ctrl->saveCriterion($this->makeCriterion('beta', 2));
+        $this->ctrl->saveCriterion($this->makeCriterion('alpha', 1));
+
+        $list = array_values($this->ctrl->getCriteriaList($this->assign->id));
+        $this->assertEquals('alpha', $list[0]->name);
+        $this->assertEquals('beta',  $list[1]->name);
+    }
+
+    public function test_duplicate_criterion_name_is_ignored(): void {
+        $this->ctrl->saveCriterion($this->makeCriterion('clarity', 1));
+        $this->ctrl->saveCriterion($this->makeCriterion('clarity', 1));
+
+        $list = $this->ctrl->getCriteriaList($this->assign->id);
+        $this->assertCount(1, $list);
+    }
+
+    public function test_update_criterion(): void {
+        $saved = $this->ctrl->saveCriterion($this->makeCriterion('clarity', 1));
+
+        $update              = $this->makeCriterion('clarity', 1);
+        $update->id          = $saved->id;
+        $update->description = 'Updated description';
+        $this->ctrl->saveCriterion($update);
+
+        $list = $this->ctrl->getCriteriaList($this->assign->id);
+        $this->assertEquals('Updated description', reset($list)->description);
+    }
+
+    public function test_delete_criterion_removes_it(): void {
+        $saved = $this->ctrl->saveCriterion($this->makeCriterion('grammar', 1));
+        $this->ctrl->deleteCriterion($saved->id, $this->assign->id);
+
+        $list = $this->ctrl->getCriteriaList($this->assign->id);
+        $this->assertCount(0, $list);
+    }
+
+    public function test_delete_all_criteria(): void {
+        $this->ctrl->saveCriterion($this->makeCriterion('alpha', 1));
+        $this->ctrl->saveCriterion($this->makeCriterion('beta', 2));
+        $this->ctrl->deleteAllCriteria($this->assign->id);
+
+        $list = $this->ctrl->getCriteriaList($this->assign->id);
+        $this->assertCount(0, $list);
+    }
+
+    // -----------------------------------------------------------------------
+    // Sort order
+    // -----------------------------------------------------------------------
+
+    public function test_change_sort_order_up_swaps_items(): void {
+        $a = $this->ctrl->saveCriterion($this->makeCriterion('alpha', 1));
+        $b = $this->ctrl->saveCriterion($this->makeCriterion('beta', 2));
+
+        $result = $this->ctrl->changeCriterionSortOrder($b->id, 'up', $this->assign->id);
+        $this->assertTrue($result);
+
+        $list = array_values($this->ctrl->getCriteriaList($this->assign->id));
+        $this->assertEquals('beta',  $list[0]->name);
+        $this->assertEquals('alpha', $list[1]->name);
+    }
+
+    public function test_change_sort_order_down_swaps_items(): void {
+        $a = $this->ctrl->saveCriterion($this->makeCriterion('alpha', 1));
+        $b = $this->ctrl->saveCriterion($this->makeCriterion('beta', 2));
+
+        $result = $this->ctrl->changeCriterionSortOrder($a->id, 'down', $this->assign->id);
+        $this->assertTrue($result);
+
+        $list = array_values($this->ctrl->getCriteriaList($this->assign->id));
+        $this->assertEquals('beta',  $list[0]->name);
+        $this->assertEquals('alpha', $list[1]->name);
+    }
+
+    public function test_change_sort_order_at_boundary_returns_false(): void {
+        $a = $this->ctrl->saveCriterion($this->makeCriterion('only', 1));
+
+        $result = $this->ctrl->changeCriterionSortOrder($a->id, 'up', $this->assign->id);
+        $this->assertFalse($result);
+    }
+
+    // -----------------------------------------------------------------------
+    // Comments
+    // -----------------------------------------------------------------------
+
+    public function test_save_and_get_comment(): void {
+        $crit = $this->ctrl->saveCriterion($this->makeCriterion('vocab', 1));
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Well done!'), $this->assign->id);
+
+        $list = $this->ctrl->getCommentList($this->assign->id);
+        $this->assertCount(1, $list);
+        $this->assertEquals('Well done!', reset($list)->comment);
+    }
+
+    public function test_duplicate_comment_is_ignored(): void {
+        $crit = $this->ctrl->saveCriterion($this->makeCriterion('vocab', 1));
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Good job!'), $this->assign->id);
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Good job!'), $this->assign->id);
+
+        $list = $this->ctrl->getCommentList($this->assign->id);
+        $this->assertCount(1, $list);
+    }
+
+    public function test_delete_comment(): void {
+        $crit = $this->ctrl->saveCriterion($this->makeCriterion('vocab', 1));
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Excellent!'), $this->assign->id);
+
+        $list       = $this->ctrl->getCommentList($this->assign->id);
+        $commentid  = (int) reset($list)->id;
+        $this->ctrl->deleteComment($commentid, $this->assign->id);
+
+        $this->assertCount(0, $this->ctrl->getCommentList($this->assign->id));
+    }
+
+    public function test_delete_criterion_also_removes_its_comments(): void {
+        $crit = $this->ctrl->saveCriterion($this->makeCriterion('vocab', 1));
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Nice!'), $this->assign->id);
+
+        $this->ctrl->deleteCriterion($crit->id, $this->assign->id);
+
+        $this->assertCount(0, $this->ctrl->getCommentList($this->assign->id));
+    }
+
+    // -----------------------------------------------------------------------
+    // Plugin data cleanup
+    // -----------------------------------------------------------------------
+
+    public function test_delete_plugin_data_removes_criteria_and_comments(): void {
+        $crit = $this->ctrl->saveCriterion($this->makeCriterion('structure', 1));
+        $this->ctrl->saveComment($this->makeComment($crit->id, 'Good structure!'), $this->assign->id);
+
+        $this->ctrl->deletePluginData($this->assign->id);
+
+        $this->assertCount(0, $this->ctrl->getCriteriaList($this->assign->id));
+        $this->assertCount(0, $this->ctrl->getCommentList($this->assign->id));
+    }
+
+    // -----------------------------------------------------------------------
+    // Helpers
+    // -----------------------------------------------------------------------
+
+    private function makeCriterion(string $name, int $sortorder): \stdClass {
+        $d                  = new \stdClass();
+        $d->id              = 0;
+        $d->assignment      = $this->assign->id;
+        $d->name            = $name;
+        $d->description     = ucfirst($name) . ' criterion';
+        $d->backgroundcolor = '#cccccc';
+        $d->sortorder       = $sortorder;
+        $d->instruction_ai  = '';
+        return $d;
+    }
+
+    private function makeComment(int $criterionid, string $text): \stdClass {
+        $c              = new \stdClass();
+        $c->id          = 0;
+        $c->criterionid = $criterionid;
+        $c->comment     = $text;
+        return $c;
+    }
+}
---
+++ src/tests/privacy_test.php
@@ -0,0 +1,187 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   assignfeedback_recitannotation
+ * @copyright 2025 RECIT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace assignfeedback_recitannotation\tests;
+
+defined('MOODLE_INTERNAL') || die();
+
+use assignfeedback_recitannotation\privacy\provider;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\types\database_table;
+
+/**
+ * Tests for the Privacy API provider.
+ *
+ * @covers \assignfeedback_recitannotation\privacy\provider
+ */
+class privacy_test extends \advanced_testcase {
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->resetAfterTest(true);
+    }
+
+    // -----------------------------------------------------------------------
+    // Metadata
+    // -----------------------------------------------------------------------
+
+    public function test_get_metadata_returns_collection(): void {
+        $collection = new collection('assignfeedback_recitannotation');
+        $result     = provider::get_metadata($collection);
+        $this->assertInstanceOf(collection::class, $result);
+    }
+
+    public function test_get_metadata_declares_annotation_table(): void {
+        $collection = new collection('assignfeedback_recitannotation');
+        $result     = provider::get_metadata($collection);
+
+        $items = $result->get_collection();
+        $this->assertArrayHasKey('assignfeedback_recitannotation', $items);
+        $this->assertInstanceOf(database_table::class, $items['assignfeedback_recitannotation']);
+    }
+
+    public function test_get_metadata_includes_required_fields(): void {
+        $collection = new collection('assignfeedback_recitannotation');
+        $result     = provider::get_metadata($collection);
+
+        $table  = $result->get_collection()['assignfeedback_recitannotation'];
+        $fields = $table->get_privacy_fields();
+
+        $this->assertArrayHasKey('submission',  $fields);
+        $this->assertArrayHasKey('ownerid',     $fields);
+        $this->assertArrayHasKey('annotation',  $fields);
+        $this->assertArrayHasKey('lastupdate',  $fields);
+    }
+
+    // -----------------------------------------------------------------------
+    // Data deletion — context level
+    // -----------------------------------------------------------------------
+
+    public function test_delete_feedback_for_context_removes_annotations(): void {
+        global $DB;
+
+        [$course, $assign, $teacher, $student, $submissionid] = $this->create_submission_fixture();
+
+        // Insert a raw annotation record directly to bypass the full React/AJAX flow.
+        $DB->insert_record('assignfeedback_recitannotation', (object)[
+            'submission' => $submissionid,
+            'ownerid'    => $teacher->id,
+            'annotation' => '<p>Test annotation</p>',
+            'occurrences'=> '{}',
+            'lastupdate' => time(),
+        ]);
+
+        $this->assertSame(1, $DB->count_records('assignfeedback_recitannotation', ['submission' => $submissionid]));
+
+        $requestdata = $this->make_request_data($assign, $student);
+        provider::delete_feedback_for_context($requestdata);
+
+        $this->assertSame(0, $DB->count_records('assignfeedback_recitannotation', ['submission' => $submissionid]));
+    }
+
+    public function test_delete_feedback_for_grade_removes_only_that_user(): void {
+        global $DB;
+
+        [$course, $assign, $teacher, $student1, $subid1] = $this->create_submission_fixture();
+
+        // Create a second student with their own submission.
+        $student2 = $this->getDataGenerator()->create_user();
+        $this->getDataGenerator()->enrol_user($student2->id, $course->id, 'student');
+        $subid2 = $this->create_raw_submission($assign->id, $student2->id);
+
+        // Insert annotations for both students.
+        foreach ([$subid1 => $student1, $subid2 => $student2] as $subid => $user) {
+            $DB->insert_record('assignfeedback_recitannotation', (object)[
+                'submission' => $subid,
+                'ownerid'    => $teacher->id,
+                'annotation' => '<p>Annotation</p>',
+                'occurrences'=> '{}',
+                'lastupdate' => time(),
+            ]);
+        }
+
+        $this->assertSame(2, $DB->count_records('assignfeedback_recitannotation'));
+
+        $requestdata = $this->make_request_data($assign, $student1);
+        provider::delete_feedback_for_grade($requestdata);
+
+        // Only student1's annotation is gone; student2's remains.
+        $this->assertSame(0, $DB->count_records('assignfeedback_recitannotation', ['submission' => $subid1]));
+        $this->assertSame(1, $DB->count_records('assignfeedback_recitannotation', ['submission' => $subid2]));
+    }
+
+    // -----------------------------------------------------------------------
+    // Helpers
+    // -----------------------------------------------------------------------
+
+    /**
+     * Creates a minimal course + assign + teacher + student + submission.
+     * Returns [$course, $assigninstance, $teacher, $student, $submissionid].
+     */
+    private function create_submission_fixture(): array {
+        $gen      = $this->getDataGenerator();
+        $course   = $gen->create_course();
+        $teacher  = $gen->create_user();
+        $student  = $gen->create_user();
+
+        $gen->enrol_user($teacher->id, $course->id, 'editingteacher');
+        $gen->enrol_user($student->id, $course->id, 'student');
+
+        $assigngen = $gen->get_plugin_generator('mod_assign');
+        $assign    = $assigngen->create_instance(['course' => $course->id]);
+
+        $subid = $this->create_raw_submission($assign->id, $student->id);
+
+        return [$course, $assign, $teacher, $student, $subid];
+    }
+
+    /**
+     * Inserts a minimal assign_submission row and returns its id.
+     */
+    private function create_raw_submission(int $assignid, int $userid): int {
+        global $DB;
+        return (int) $DB->insert_record('assign_submission', (object)[
+            'assignment'    => $assignid,
+            'userid'        => $userid,
+            'timecreated'   => time(),
+            'timemodified'  => time(),
+            'status'        => 'submitted',
+            'groupid'       => 0,
+            'attemptnumber' => 0,
+            'latest'        => 1,
+        ]);
+    }
+
+    /**
+     * Builds an assign_plugin_request_data mock for the given assign + user.
+     */
+    private function make_request_data(\stdClass $assigninstance, \stdClass $user): \mod_assign\privacy\assign_plugin_request_data {
+        global $DB, $CFG;
+        require_once($CFG->dirroot . '/mod/assign/locallib.php');
+
+        $cm     = get_coursemodule_from_instance('assign', $assigninstance->id);
+        $context = \context_module::instance($cm->id);
+        $assign  = new \assign($context, $cm, null);
+
+        return new \mod_assign\privacy\assign_plugin_request_data($context, $assign, null, [], $user);
+    }
+}
